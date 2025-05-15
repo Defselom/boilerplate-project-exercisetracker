a2f9e89bd6c2d4252448658e8b9619fbaed2051b
defselom <selomadzomla@gmail.com>
2025-05-15 14:23:08 +0000

    🐳 chore(package): add body-parser and mongoose

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
-
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors())
 app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+
 
 
 
