6bf034bcdea6be0258e1f5e4c3e239774025bded
defselom <selomadzomla@gmail.com>
2025-05-15 16:12:53 +0000

    ✨ feat(users): handle users

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-require('dotenv').config()
 const bodyParser = require('body-parser');
+const Database = require('./config/db');
+const { userModel } = require('./schema/userSchema');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors())
@@ -12,10 +13,27 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+(async () => {
+  await Database.connect();
+})();
 
+app.post('/api/users', async (req, res) => {
+  const { username } = req.body;
+  if (!username) {
+    return res.json({ error: "Username not found" })
+  }
 
+  let newUserName = new userModel({ username: username });
 
+  newUserName = await newUserName.save();
 
+  return res.json(newUserName)
+});
+
+app.get('/api/users', async (req, res) => {
+  const users = await userModel.find();
+  return res.json(users)
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
